31a3f19015613bdcacccd5f87ae9add382832f45
Jonathan Siegel <248302+usiegj00@users.noreply.github.com>
2026-02-22 22:41:16 +0900

    Strip quarantine attribute in postflight

--- Casks/agent-solutions.rb
+++ Casks/agent-solutions.rb
@@ -1,6 +1,6 @@
 cask "agent-solutions" do
   version "0.1.1"
-  sha256 "a9534930adff6279c80ebee5780edd544b4a6c6e70775d02dfab300f7872d377"
+  sha256 "PLACEHOLDER"
 
   url "https://github.com/aluminumio/homebrew-agent-solutions/releases/download/v#{version}/AgentSolutions-#{version}-arm64.dmg",
       verified: "github.com/aluminumio/homebrew-agent-solutions/"
@@ -16,4 +16,10 @@ cask "agent-solutions" do
   end
 
   app "Agent Solutions.app"
+
+  postflight do
+    system_command "/usr/bin/xattr",
+                   args: ["-rd", "com.apple.quarantine", "#{appdir}/Agent Solutions.app"],
+                   sudo: false
+  end
 end
